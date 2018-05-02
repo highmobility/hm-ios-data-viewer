f275bb863f36ca3336e36592c24268ad87f5561d
doofyus <mikk.ratsep@gmail.com>
2018-05-02 16:03:09 +0300

    Initial Commit

---
+++ Data Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Data Viewer
+//
+//  Created by Mikk Rätsep on 02/05/2018.
+//  Copyright © 2018 High-Mobility OÜ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
